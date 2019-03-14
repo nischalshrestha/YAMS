44ff8d6c04fc9273adf94c68bb1c805d2ea05efd
nischalshrestha <bitsorific@gmail.com>
2019-03-14 18:14:14 -0400

    encapsulate sine wave generation in method, tobytes for pyaudio

--- sine.py
+++ sine.py
@@ -1,24 +1,25 @@
 import pyaudio
 import numpy as np
+import time
 
-# Note: this blocks
+# Note: this blocks main thread
 
 p = pyaudio.PyAudio()
 
-amp = 0.5     # range [0.0, 1.0]
 fr = 44100       # sampling rate, Hz, must be integer
-duration = 1.0   # in seconds, may be float
-f = 440.0        # sine frequency, Hz, may be float
 
-start = 0
-offset = 0
-
-# framerate is the # samples per second so we divide each second by fr
-ts = start + np.arange(fr * duration)
-# This is the evaluation of the wave given the ts
-phases = 2 * np.pi * f * ts  + offset
-# generate samples, note conversion to float32 array
-ys = (amp * np.sin(phases)).astype(np.float32).tobytes()
+def generate_wave(freq=440, duration=1.0, start=0, offset=0, amp=1.0):
+    # makes sure that amp is within range [0.0, 1.0]
+    amp = max(0, min(1.0, amp))
+    # framerate is the # samples per second so we divide each second by fr
+    ts = start + np.arange(fr * duration) / fr
+    # This is the evaluation of the wave given the ts
+    phases = 2 * np.pi * freq * ts  + offset
+    # generate samples, note conversion to float32 array
+    # the tobytes() is required due to pyaudio conversion of numpy
+    # https://stackoverflow.com/a/48454913/9193847
+    ys = (amp * np.sin(phases).astype(np.float32))
+    return ys.tobytes()
 
 # for paFloat32 sample values must be in range [-1.0, 1.0]
 stream = p.open(format=pyaudio.paFloat32,
@@ -27,7 +28,7 @@ stream = p.open(format=pyaudio.paFloat32,
                 output=True)
 
 # play. May repeat with different volume values (if done interactively) 
-stream.write(ys)
+stream.write(generate_wave(freq=440, amp=0.9))
 
 stream.stop_stream()
 stream.close()
