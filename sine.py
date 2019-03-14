7f58349c0c901a9e6ed82f38ea6c6accbc0d4d89
nischalshrestha <bitsorific@gmail.com>
2019-03-14 15:14:10 -0400

    porting music theory constants to Python

--- sine.py
+++ sine.py
@@ -14,11 +14,11 @@ start = 0
 offset = 0
 
 # framerate is the # samples per second so we divide each second by fr
-ts = start + np.arange(fr * duration) / fr
+ts = start + np.arange(fr * duration)
 # This is the evaluation of the wave given the ts
 phases = 2 * np.pi * f * ts  + offset
 # generate samples, note conversion to float32 array
-ys = amp * (np.sin(phases)).astype(np.float32)
+ys = (amp * np.sin(phases)).astype(np.float32).tobytes()
 
 # for paFloat32 sample values must be in range [-1.0, 1.0]
 stream = p.open(format=pyaudio.paFloat32,
