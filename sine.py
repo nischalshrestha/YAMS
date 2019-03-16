aa4bd15bcfb838d49a4090e987e669a0aca3f574
nischalshrestha <bitsorific@gmail.com>
2019-03-16 17:46:28 -0400

    added non blocking approach to playing audio stream with callback

--- sine.py
+++ sine.py
@@ -2,11 +2,11 @@ import pyaudio
 import numpy as np
 import time
 
-# Note: this blocks main thread
+# TODO add encapsulation with use of classes once different wave shapes
+# can be generated
 
 p = pyaudio.PyAudio()
-
-fr = 44100       # sampling rate, Hz, must be integer
+fr = 44100
 
 def generate_wave(freq=440, duration=1.0, start=0, offset=0, amp=1.0):
     # makes sure that amp is within range [0.0, 1.0]
@@ -18,17 +18,37 @@ def generate_wave(freq=440, duration=1.0, start=0, offset=0, amp=1.0):
     # generate samples, note conversion to float32 array
     # the tobytes() is required due to pyaudio conversion of numpy
     # https://stackoverflow.com/a/48454913/9193847
-    ys = (amp * np.sin(phases).astype(np.float32))
-    return ys.tobytes()
+    ys = amp * np.sin(phases).astype(np.float32)
+    return ys
+
+sine_wave = generate_wave(freq=440, amp=0.9)
+
+start_offset = 0
+def callback(in_data, frame_count, time_info, status):
+    global start_offset # not a fan of global var but works for now
+    end = start_offset + frame_count
+    data = sine_wave[start_offset:end]
+    start_offset += frame_count
+    # let pyaudio continue calling this function until there's no more data
+    # to be read from wave
+    return (data, pyaudio.paContinue)
 
 # for paFloat32 sample values must be in range [-1.0, 1.0]
 stream = p.open(format=pyaudio.paFloat32,
                 channels=1,
                 rate=fr,
-                output=True)
+                output=True,
+                stream_callback=callback)
 
-# play. May repeat with different volume values (if done interactively) 
-stream.write(generate_wave(freq=440, amp=0.9))
+# blocking version uses this and removes the stream_callback parameter in open()
+# stream.write(sine_wave)
+
+# non-blocking
+# start the stream
+stream.start_stream()
+# wait for stream to finish because the audio playing is non-blocking
+while stream.is_active():
+    time.sleep(0.1)
 
 stream.stop_stream()
 stream.close()
