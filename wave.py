92a4846c0351a17e8c1677c395c9867f8c10b3e0
nischalshrestha <bitsorific@gmail.com>
2019-03-24 12:44:04 -0400

    adding triangle wave function

--- wave.py
+++ wave.py
@@ -1,14 +1,49 @@
 import pyaudio
 import numpy as np
+from scipy import signal
+import matplotlib.pyplot as plt
+
+t = np.linspace(0, 1, 500)
+plt.plot(t, signal.sawtooth(2 * np.pi * 5 * t))
+
 import time
 
 # TODO add encapsulation with use of classes once different wave shapes
 # can be generated
 
 p = pyaudio.PyAudio()
-fr = 44100
+# fr = 44100
+fr = 10000
 
-def generate_wave(freq=440, duration=1.0, start=0, offset=0, amp=1.0):
+# source: https://github.com/AllenDowney/ThinkDSP/blob/master/code/thinkdsp.py#L1068
+def normalize(ys, amp=1.0):
+    """Normalizes a wave array so the maximum amplitude is +amp or -amp.
+    ys: wave array
+    amp: max amplitude (pos or neg) in result
+    returns: wave array
+    """
+    high, low = abs(max(ys)), abs(min(ys))
+    return amp * ys / max(high, low)
+
+# source: https://github.com/AllenDowney/ThinkDSP/blob/master/code/thinkdsp.py#L1058
+def unbias(ys):
+    """Shifts a wave array so it has mean 0.
+    ys: wave array
+    returns: wave array
+    """
+    return ys - ys.mean()
+
+def generate_triangle(freq=440, duration=1.0, start=0, offset=0, amp=1.0):
+    # makes sure that amp is within range [0.0, 1.0]
+    amp = max(0, min(1.0, amp))
+    ts = start + np.arange(fr * duration) / fr
+    cycles = freq * ts + offset / 2*np.pi
+    frac, _ = np.modf(cycles)
+    ys = np.abs(frac - 0.5)
+    ys = normalize(unbias(ys), amp)
+    return ys.astype(np.float32)
+
+def generate_sine(freq=440, duration=1.0, start=0, offset=0, amp=1.0):
     # makes sure that amp is within range [0.0, 1.0]
     amp = max(0, min(1.0, amp))
     # framerate is the # samples per second so we divide each second by fr
@@ -16,22 +51,23 @@ def generate_wave(freq=440, duration=1.0, start=0, offset=0, amp=1.0):
     # This is the evaluation of the wave given the ts
     phases = 2 * np.pi * freq * ts  + offset
     # generate samples, note conversion to float32 array
-    # the tobytes() is required due to pyaudio conversion of numpy
-    # https://stackoverflow.com/a/48454913/9193847
     ys = amp * np.sin(phases).astype(np.float32)
     return ys
 
-sine_wave = generate_wave(freq=440, amp=0.9)
+sine_wave = generate_sine(freq=200)
+triangle_wave = generate_triangle(freq=200)
 
 def callback(in_data, frame_count, time_info, status):
     end = callback.start_offset + frame_count
-    data = sine_wave[callback.start_offset:end]
+    data = callback.wave[callback.start_offset:end]
     callback.start_offset += frame_count
     # let pyaudio continue calling this function until there's no more data
     # to be read from wave
     return data, pyaudio.paContinue
 
 callback.start_offset = 0
+# callback.wave = sine_wave
+callback.wave = triangle_wave
 
 # for paFloat32 sample values must be in range [-1.0, 1.0]
 stream = p.open(format=pyaudio.paFloat32,
@@ -42,6 +78,7 @@ stream = p.open(format=pyaudio.paFloat32,
 
 # blocking version uses this and removes the stream_callback parameter in open()
 # stream.write(sine_wave)
+# stream.write(triangle_wave)
 
 # non-blocking
 # start the stream
